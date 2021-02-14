d2cc04575fc16a60d5d3d0d20c62d53a92b196cc
Filipe dos Santos Gundim <filipe@admooh.com>
2021-02-14 17:13:28 -0300

    implements exercicio3

--- src/lista1/exercicio3.java
+++ src/lista1/exercicio3.java
@@ -1,5 +1,30 @@
 package lista1;
 
+/*3. Carregar um vetor [100] inteiros positivos ou negativos. Classificar este vetor em ordem crescente e apresentar os valores.*/
+
 public class exercicio3 {
+	public static void main(String[] args) {
+		int[] vetor = new int[100];
+
+		for (int i = 0; i <= vetor.length; i++) {
+			vetor[i] = 1 + (int) (Math.random() * 100);
+		}
+
+		System.out.println(order(vetor));
+	}
+
+	static int[] order(int[] vetor) {
+		int aux = 0;
+		for (int i = 0; i < vetor.length; i++) {
+			for (int j = 0; j < 4; j++) {
+				if (vetor[j] > vetor[j + 1]) {
+					aux = vetor[j];
+					vetor[j] = vetor[j + 1];
+					vetor[j + 1] = aux;
+				}
+			}
+		}
+		return vetor;
+	}
 
 }
